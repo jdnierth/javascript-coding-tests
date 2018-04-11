a85f11d5dfda729c5a0907e8173d58f4c8a2c860
Jessica Nierth <jnierth@web.de>
2018-04-12 08:00:28 +1000

    Revision of exercise 1 of asynchronous JavaScript.

--- asynchronous-javascript/ex1/ex1-fixed-1.js
+++ asynchronous-javascript/ex1/ex1-fixed-1.js
@@ -19,20 +19,43 @@ function output(text) {
 
 // **************************************
 // The old-n-busted callback way
+var responses = {};
 
 function getFile(file, cb) {
 	fakeAjax(file,function(text){
-		if(typeof cb === 'function') {
-			cb();
-		}
+		orderResponses(file, text);
 	});
 }
 
+function orderResponses(filename, content) {
+
+	// Add all fake responses to a variable for reference
+	if(!responses[filename]) {
+		responses[filename] = content;
+	}
+
+	// The order in which the files have to be render with
+	var order = ["file1", "file2", "file3"];
+
+	for(var i = 0, len = order.length; i < len; i++) {
+
+		// If the response has been found in the order array, print it.
+		if(order[i] in responses) {
+			if(responses[order[i]] !== true) {
+				// Print the file.
+				output(responses[order[i]]);
+				// Set a flag to differentiate if a file has been rendered
+				responses[order[i]] = true;
+            }
+		} else {
+			return;
+		}
+	}
+
+	output('Complete!');
+}
+
 // request all files at once in "parallel"
-getFile("file1", function() {
-    getFile("file2", function() {
-        getFile("file3", function() {
-        	output('Complete!');
-		});
-    });
-});
+getFile("file1");
+getFile("file2");
+getFile("file3");
