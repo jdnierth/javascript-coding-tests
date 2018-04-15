1abaa8309441f5ee7154c8653f18f9e34664bdc6
Jessica Nierth <jnierth@web.de>
2018-04-16 06:43:50 +1000

    Added results from exercise 3 of asynchronous javascript

---
+++ asynchronous-javascript/ex3/ex3-jes.js
@@ -0,0 +1,41 @@
+function fakeAjax(url,cb) {
+	var fake_responses = {
+		"file1": "The first text",
+		"file2": "The middle text",
+		"file3": "The last text"
+	};
+	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+
+	console.log("Requesting: " + url);
+
+	setTimeout(function(){
+		cb(fake_responses[url]);
+	},randomDelay);
+}
+
+function output(text) {
+	console.log(text);
+}
+
+// **************************************
+
+function getFile(file) {
+	return new Promise(function writeFiles(resolve, reject) {
+		return fakeAjax(file, resolve);
+	});
+}
+// request all files at once in "parallel"
+var f1 = getFile('file1');
+var f2 = getFile('file2');
+var f3 = getFile('file3');
+
+f1
+	.then(output)
+	.then(function() {
+		return p2;
+	})
+	.then(output)
+	.then(function() {
+		return p3;
+	})
+	.then(output);
