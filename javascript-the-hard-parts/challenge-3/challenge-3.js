127e36e9cc1d1a97d5765c412857e3198cba71d6
Jessica Nierth <jnierth@web.de>
2018-04-30 19:30:30 +1000

    (feat) Added third challenge of the course 'JavaScript - The hard parts' from frontendmasters.

---
+++ javascript-the-hard-parts/challenge-3/challenge-3.js
@@ -0,0 +1,19 @@
+(function() {
+
+    function _map(arrayOfNumbers,cb){
+        var newMap = [];
+
+        for(var i = 0; i < arrayOfNumbers.length; i++) {
+            newMap.push(multiplyByTwo(arrayOfNumbers[i]));
+        }
+
+        return newMap;
+    }
+
+    function multiplyByTwo(x) {
+        return x * 2;
+    }
+
+    console.log('_map([1,2,3,4,5]): ',_map([1,2,3,4,5]));
+
+})();
