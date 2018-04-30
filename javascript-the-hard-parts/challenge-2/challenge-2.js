1f6026b19d270f02275c3c6ce7416a6a48bd1369
Jessica Nierth <jnierth@web.de>
2018-04-30 19:30:09 +1000

    (feat) Added second challenge of the course 'JavaScript - The hard parts' from frontendmasters.

---
+++ javascript-the-hard-parts/challenge-2/challenge-2.js
@@ -0,0 +1,11 @@
+(function() {
+
+
+    function addS(x) {
+        return x + "s";
+    }
+
+    console.log('addS(1): ',addS(1));
+    console.log('addS("Hallo you"): ',addS("Hello you"));
+
+})();
