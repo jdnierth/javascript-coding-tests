3e4602951b6c8dc238b3a26010e44bb6acb126bf
Jessica Nierth <jnierth@web.de>
2018-05-05 18:19:00 +1000

    (feat) Solution to challenge 10 of 'JavaScript the hard parts' - frontendmasters.

---
+++ javascript-the-hard-parts/challenge-9/challenge-9.js
@@ -0,0 +1,24 @@
+"use strict";
+
+(function () {
+    function multiMap(arrVals, arrCallbacks) {
+        var res = {};
+
+        arrVals.map(function(el) {
+            res[el] = arrCallbacks.map(function(cb) {
+                return cb(el);
+            });
+        });
+
+        return res;
+    }
+
+    console.log(multiMap(['catfood', 'glue', 'beer'], [function (str) {
+        return str.toUpperCase();
+    }, function (str) {
+        return str[0].toUpperCase() + str.slice(1).toLowerCase();
+    }, function (str) {
+        return str + str;
+    }]));
+// should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
+})();
