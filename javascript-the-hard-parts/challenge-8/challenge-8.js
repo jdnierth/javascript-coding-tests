b4c98c42649db68ae36a02a6698bd654f3457640
Jessica Nierth <jnierth@web.de>
2018-05-05 18:06:18 +1000

    (feat) Solution for challenge 8 of 'JavaScript the hard parts' frontendmasters.

---
+++ javascript-the-hard-parts/challenge-8/challenge-8.js
@@ -0,0 +1,26 @@
+"use strict";
+
+(function () {
+
+    function objOfMatches(array1, array2, callback) {
+        var res =  {};
+
+        for(var i = 0; i < array1.length; i++) {
+            var upper = callback(array1[i]);
+
+            if(array2 && upper === array2[i]) {
+                res[array1[i]] = array2[i];
+            }
+        }
+
+        return res;
+    }
+
+    function transform(str) {
+        return str.toUpperCase();
+    }
+
+    console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], transform));
+// should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
+
+})();
