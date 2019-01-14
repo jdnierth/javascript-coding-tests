a7ad8e30acd4b63d741abd6ae5e4b8024d16d939
Jessica Nierth <jnierth@web.de>
2019-01-14 18:14:00 +1100

    feat (Added solution for insertion sort)

---
+++ computer-science-in-4-semesters/challenge-3-insertion-sort/challenge-3.js
@@ -0,0 +1,59 @@
+(function insertionSort() {
+    var count = 0,
+        inputArray = [6, 1, 3, 5, 4, 2],
+        result = insertionSort(inputArray);
+
+    console.log('Input: ', inputArray, ' result: ', result, ' iteration: ', count);
+
+    ///////////////////////////////
+
+    function insertionSort(nums) {
+        var outputArray = [];
+        console.log('MAX LENGTH: ', nums.length);
+
+        for (var i = 0, length = nums.length; i < length; i++) {
+
+            if(i === 0) {
+                outputArray.push(nums[i]);
+            }
+
+            outputArray = insertNewNumber(outputArray, nums[i]);
+            count = count + 1;
+        }
+
+        return outputArray;
+    }
+
+    function insertNewNumber(outputArray, newNumber) {
+        for(var i = 0, len = outputArray.length; i < len; i++) {
+            if(outputArray[i] > newNumber) {
+                outputArray.splice(i,0,newNumber);
+                break;
+            }
+        }
+
+        return outputArray;
+
+    }
+
+    /**
+     * Checks if an array is sorted.
+     *
+     * @param {array} nums array of numbers
+     * @returns {boolean} returns true if array is sorted else false.
+     */
+    function isSorted(nums) {
+        var length = nums.length;
+
+        for (var i = 0; i < length; i++) {
+            if ((i + 1) <= length) {
+                if (nums[i] > nums[i + 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+}());
