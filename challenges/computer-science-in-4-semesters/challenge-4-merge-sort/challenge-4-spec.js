11648166b9f6394c43aeff032d3c85d2956e1be3
Jessica Nierth <jnierth@web.de>
2019-01-16 11:07:20 +1100

    feat (Added Jasmine test cases to mergeSort challenge)

---
+++ challenges/computer-science-in-4-semesters/challenge-4-merge-sort/challenge-4-spec.js
@@ -0,0 +1,25 @@
+// unit tests
+// do not modify the below code
+describe('merge sort', function () {
+    it('should sort correctly', function (done) {
+        var nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+
+        nums = mergeSort.mergeSort(nums);
+
+        expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+        done();
+    });
+
+    it('lists should be merged in ascending order', function(done) {
+        var nums1 = [3,5,8,9];
+        var nums2 = [1,2,4,6,7];
+
+        var nums = mergeSort.mergeLists(nums1,nums2);
+
+        expect(nums).toEqual([1,2,3,4,5,6,7,8,9]);
+
+        done();
+
+    })
+});
