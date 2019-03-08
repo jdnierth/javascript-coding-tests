70f59a4c02b4cdb09d6391580d0c69ee33242ab9
Jessica Nierth <jnierth@web.de>
2019-03-08 09:07:34 +0100

    refactor (Code clean up and renaming folder identifying implementation strategy)

---
+++ challenges/a-practical-guide-to-algorithms-with-javascript/exercise-5-factorial-iterative/exercise-5.js
@@ -0,0 +1,29 @@
+var factorial = (function factorial() {
+
+    // Multiply
+    function multiply(n,m) {
+        return n * m;
+    }
+
+    function factorial(n) {
+
+        var result = 1;
+        
+        if(n < 0) {
+            throw new Error('Number has to be > 0');
+        }
+
+        for(var i = n; i >= 1; i--) {
+            result = multiply(i, result);
+        }
+
+        return result;
+    }
+
+    factorial(4);
+
+    return {
+        factorial: factorial,
+        multiply: multiply
+    }
+})();
