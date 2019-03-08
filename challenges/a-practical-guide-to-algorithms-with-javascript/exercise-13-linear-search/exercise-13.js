e77d16e56f66b45599d6d8d61b120c5b0b56dfb3
Jessica Nierth <jnierth@web.de>
2019-03-08 20:05:09 +0100

    feat (Added solution for linear search)

--- challenges/a-practical-guide-to-algorithms-with-javascript/exercise-13-linear-search/exercise-13.js
+++ challenges/a-practical-guide-to-algorithms-with-javascript/exercise-13-linear-search/exercise-13.js
@@ -1,6 +1,23 @@
 var linearSearch = (function linearSearch() {
 
-   
+    /**
+     * Loops through a list of elements one by one until it finds the element the user searches for.
+     *
+     * @param {Array} elements that needs to be searched through (aka 'haystack').
+     * @param {Number} n the element the user wants to search for (aka 'needle').
+     *
+     * @return {Number} i the index on which the element n was found within elements.
+     */
+    function linearSearch(elements, n) {
+        for(var i = 0, len = elements.length; i < len; i++) {
+            if(elements[i] == n) {
+                return i;
+            }
+        }
+
+        throw new Error('The element could not be found within the given dataset.');
+    }
+
     return {
         linearSearch: linearSearch
     }
--- challenges/a-practical-guide-to-algorithms-with-javascript/exercise-13-linear-search/exercise-13.spec.js
+++ challenges/a-practical-guide-to-algorithms-with-javascript/exercise-13-linear-search/exercise-13.spec.js
@@ -1,6 +1,21 @@
 // unit tests
 describe('LinearSearch', function () {
-    it('XXX', function (done) {
+    it('LinearSearch returns the index of the searched number that was found within the dataset', function (done) {
+        expect(linearSearch.linearSearch([5,1,2,3,7],3)).toEqual(3);
+        expect(linearSearch.linearSearch([5,1,2,3,7],5)).toEqual(0);
+
+        done();
+    });
+
+    it('LinearSearch function throws an error if no element could be found', function(done) {
+        expect(function() {
+            linearSearch.linearSearch([5,1,2,3,7],10)
+        }).toThrow(new Error('The element could not be found within the given dataset.'));
+        done();
+    });
+
+    it('LinearSearch returns the first index if there are multiple items', function(done) {
+        expect(linearSearch.linearSearch([5,1,2,3,7],5)).toEqual(0);
 
         done();
     });
