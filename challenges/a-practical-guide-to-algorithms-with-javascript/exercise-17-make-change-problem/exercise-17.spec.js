7dab015dee697a810ba0131d5ad13b9db1ad82e8
Jessica Nierth <jnierth@web.de>
2019-03-09 16:02:48 +0100

    feat (Added alternative solution to makeChange task)

--- challenges/a-practical-guide-to-algorithms-with-javascript/exercise-17-make-change-problem/exercise-17.spec.js
+++ challenges/a-practical-guide-to-algorithms-with-javascript/exercise-17-make-change-problem/exercise-17.spec.js
@@ -16,3 +16,20 @@ describe('MakeChange', function () {
     });
 
 });
+describe('MakeChange2', function () {
+    it('Sort numbers in ascending order', function (done) {
+        expect(makeChange2.sortDescending([11, 1, 9, 8, 2])).toEqual([11, 9, 8, 2, 1]);
+        expect(makeChange2.sortDescending([])).toEqual([]);
+        expect(makeChange2.sortDescending([0, -10, 2, 3, 4])).toEqual([4, 3, 2, 0, -10]);
+
+        done();
+    });
+
+    it('MakeChange with the least amount of variations', function (done) {
+        expect(makeChange2.makeChange([], 40)).toEqual([]);
+        expect(makeChange2.makeChange([5, 10, 25], 40)).toEqual([25, 10, 5]);
+
+        done();
+    });
+
+});
