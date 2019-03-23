5d84792876969864767114a7ec3c4fba50fd11de
Jessica Nierth <jnierth@web.de>
2019-03-23 09:35:08 +0100

    feat (Addedfurther exercises to the custom foreach loop)

--- challenges/javascript-ninja/foreach/myforeach.js
+++ challenges/javascript-ninja/foreach/myforeach.js
@@ -7,6 +7,9 @@
 var myForeach = (function myForeach() {
 
     function myForeach(list, callback) {
+        // TODO: What if the first element is not an array
+        // TODO: What if the 2nd argument is not a function
+        // TODO: How would you implement this if the author would want to stop at any index within the loop?
         for(var i = 0; i < list.length; i++) {
             // Mithilfe der call()-Methode der Callback-Funktion wird das aktuelle Iterationselement als erster und der Schleifenindex als zweiter Paramter [bergeben. Damit sollte das aktuelle Element zum Funktionskontext und der Index als einziger Parameter an das Callback weitergegeben werden.
             callback.call(list[i],i);
