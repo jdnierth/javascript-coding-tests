3b61cd5db2685c31f75f2e7e88b0390623ca1fe4
Jessica Nierth <jnierth@web.de>
2019-03-23 17:50:41 +0100

    feat (Alternative solution for palindrome added)

--- challenges/javascript-ninja/palindrome/Palindrome.js
+++ challenges/javascript-ninja/palindrome/Palindrome.js
@@ -30,3 +30,18 @@ function isPalindrome(word, n) {
 		}
 	}
 }
+
+/**
+ * Alternative isPalindrome text where an updated version of the string will be passed on to the recursive call
+ * making it unnecessary to pass through the index.
+ *
+ * @param {string} text the text to be tested for being a palindrome or not.
+ *
+ * @returns {*}
+ */
+function isPalindrome2(text) {
+	// TODO: test for null or undefined
+	if(text.length <= 1) return true;
+	if(text.charAt(0) != text.charAt(text.length-1)) return false;
+	return isPalindrome(text.substr(1), text.length - 2);
+ }
