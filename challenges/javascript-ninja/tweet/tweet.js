957ea2c071120ec136b97bec5500dd949576ae75
Jessica Nierth <jnierth@web.de>
2019-03-23 17:56:58 +0100

    feat (Added version of a recursive call by using an inline function)

--- challenges/javascript-ninja/tweet/tweet.js
+++ challenges/javascript-ninja/tweet/tweet.js
@@ -21,3 +21,26 @@ person = {};
 
 // this bezieht sich auf person2!
 person2.tweet(3);
+
+
+/**
+ * Ansatz als inline function
+ */
+
+var person3 = {
+    //if you give an anonymous function a name it is called an inline function (named function 'tweetme')
+    // and can be referenced for recursive calls.
+    tweet: function tweetme(n) {
+        return (n > 1) ? tweetme(n - 1) + '-tweet' : 'tweet';
+    }
+};
+
+person3.tweet(2);
+
+var person4 = {
+    tweet: person3.tweet
+};
+
+person3 = {};
+
+person4.tweet(4);
