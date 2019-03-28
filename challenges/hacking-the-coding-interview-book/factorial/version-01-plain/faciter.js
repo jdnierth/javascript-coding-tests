aca82b3f06cfccbcb5ed59e55629a13db92df6e3
Jessica Nierth <jnierth@web.de>
2019-03-28 23:24:56 +0100

    feat (Added solution for factorial implentation as an iteration or recursion)

---
+++ challenges/hacking-the-coding-interview-book/factorial/version-01-plain/faciter.js
@@ -0,0 +1,23 @@
+/**
+ * Calculate factorial with an iterative approach.
+ *
+ * n! = f(n) * f(n-1);
+ *
+ * n = 0: 1
+ * n = 1: 1
+ * n = 2: 3
+ * n = 3: 6
+ *
+ * @author: Jessica Nierth
+ * @date: 16.03.2019
+ */
+var faciter = (function faciter() {
+
+    function faciter(n) {
+    }
+
+    return {
+        faciter: faciter
+    }
+
+});
---
+++ challenges/hacking-the-coding-interview-book/factorial/version-01-plain/facrek.js
@@ -0,0 +1,20 @@
+/**
+ * SOME DESCRIPTION
+ *
+ * @author: Jessica Nierth
+ * @date: 15.03.2019
+ */
+var fibrek = (function fibrek() {
+
+    function fibrek(n) {
+    }
+
+    return {
+        fibrek: fibrek
+    }
+
+});
+
+/*
+* Big O is O(n^2) as there are 2 functions are being called for each n.
+*/
