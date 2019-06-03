877cd6efa7a76fe9e745986b2996d56ef03fe99e
Jessica Nierth <jnierth@web.de>
2019-06-03 14:28:15 +0200

    feat (Added comments on the JavaScript built in iterator pattern)

--- challenges/javascript-the-new-hard-parts/challenge-02-iterator/iterator.js
+++ challenges/javascript-the-new-hard-parts/challenge-02-iterator/iterator.js
@@ -11,9 +11,21 @@ function createFunction(list) {
 }
 
 var returnNextElement = createFunction([4,5,6]);
-var elem1 = returnNextElement();
-var elem2 = returnNextElement();
+var elem1 = returnNextElement.next();
+var elem2 = returnNextElement.next();
 
+// INFO:
+// JavaScripts built in iterators are actually objects with a next method that when called returns the next element
+// from the "stream" or "flow"
 // Uses closure to keep track of the index and close over the list variable.
-
+// The built in iterators actually produce the next element in the format:
+// {
+//    value: 4,
+//	  done: false
+// }
+//  or
+// {
+//    value: undefined,
+//	  done: true
+// }
 
