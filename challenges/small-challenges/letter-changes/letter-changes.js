9bafef114333a58273b701291b83bb8e123baf48
Jessica Nierth <jnierth@web.de>
2019-06-16 20:40:15 +0200

    refactor (Solution for challenge letter changer)

--- challenges/small-challenges/letter-changes/letter-changes.js
+++ challenges/small-challenges/letter-changes/letter-changes.js
@@ -10,90 +10,25 @@ function letterChange(sentence) {
 }
 
 function letterTransformation(currentValue, index) {
-    var tmp = currentValue.toLowerCase();
+    if (currentValue.trim() != '') {
 
-    switch(tmp) {
-        case 'a':
-            currentValue = 'b';
-            break;
-        case 'b':
-            currentValue = 'c';
-            break;
-        case 'c':
-            currentValue = 'd';
-            break;
-        case 'd':
-            currentValue = 'E';
-            break;
-        case 'e':
-            currentValue = 'f';
-            break;
-        case 'f':
-            currentValue = 'g';
-            break;
-        case 'g':
-            currentValue = 'h';
-            break;
-        case 'h':
-            currentValue = 'I';
-            break;
-        case 'i':
-            currentValue = 'j';
-            break;
-        case 'j':
-            currentValue = 'k';
-            break;
-        case 'k':
-            currentValue = 'l';
-            break;
-        case 'l':
-            currentValue = 'm';
-            break;
-        case 'm':
-            currentValue = 'n';
-            break;
-        case 'n':
-            currentValue = 'O';
-            break;
-        case 'o':
-            currentValue = 'p';
-            break;
-        case 'p':
-            currentValue = 'q';
-            break;
-        case 'q':
-            currentValue = 'r';
-            break;
-        case 'r':
-            currentValue = 's';
-            break;
-        case 's':
-            currentValue = 't';
-            break;
-        case 't':
-            currentValue = 'U';
-            break;
-        case 'u':
-            currentValue = 'v';
-            break;
-        case 'v':
-            currentValue = 'w';
-            break;
-        case 'w':
-            currentValue = 'x';
-            break;
-        case 'x':
-            currentValue = 'y';
-            break;
-        case 'y':
-            currentValue = 'z';
-            break;
-        case 'z':
-            currentValue = 'A';
-            break;
+        // Returns the character code of the current value
+        var newCharCode = (currentValue.charCodeAt() + 1),
+            newCharacter = String.fromCharCode(newCharCode);
+
+        switch (newCharacter) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                newCharacter = newCharacter.toUpperCase();
+                break;
+        }
+    } else {
+        return currentValue;
     }
-
-    return currentValue;
+    return newCharacter;
 }
 
 console.log(letterChange('hello there'));
