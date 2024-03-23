d452f7a0959ab458ab483e8f56a984035500582b
jdmeinhold <jdmeinhold@web.de>
2024-03-23 16:43:30 +0100

    Removed introduction to node

--- challenges/introduction-to-node-js/original/exercises/cli/prompts.js
+++
@@ -1,7 +0,0 @@
-const newContactPrompts = [
-  {name: 'firstName', message: 'First Name'},
-  {name: 'lastName', message: 'Last Name'},
-  {name: 'phoneNumber', message: 'Phone Number'}
-]
-
-module.exports = {newContactPrompts}
--- challenges/introduction-to-node-js/original/exercises/testing/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('./api')
-
-app.listen(3001, () => {
-  console.log(`server on @http://localhost:3001`)
-})
